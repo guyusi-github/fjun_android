5cee1a7fa6d5801c9a5c40681d48abbbf60c7a1b
guyusi <1903292337@qq.com>
2020-05-31 20:41:27 +0800

    first commit

---
+++ NotePad-master/app/src/main/java/com/example/android/notepad/SearchActivity.java
@@ -0,0 +1,46 @@
+package com.example.android.notepad;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+public class SearchActivity  extends Activity {
+    private String[] mStrs = {"<未知名>", "aaa", "ccc", "airsaid"};
+    private SearchView mSearchView;
+    private ListView mListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_note);
+        mSearchView = (SearchView) findViewById(R.id.searchView);
+        mListView = (ListView) findViewById(R.id.listView);
+        mListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mStrs));
+        mListView.setTextFilterEnabled(true);
+
+        // 设置搜索文本监听
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            // 当点击搜索按钮时触发该方法
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            // 当搜索内容改变时触发该方法
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (!TextUtils.isEmpty(newText)){
+                    mListView.setFilterText(newText);
+                }else{
+                    mListView.clearTextFilter();
+                }
+                return false;
+            }
+        });
+
+    }
+}
+
